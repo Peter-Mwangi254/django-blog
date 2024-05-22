dcb991514bc17153a7f5aa351a9cd0c38be42b77
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-22 23:59:18 +0300

    Added template tag for article_list

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
