5de0e1a7089c61181c7ce3387a57e3df2276f06c
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-22 21:21:02 +0300

    added the django admin functionality

---
+++ blog/articles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
