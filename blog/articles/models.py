2d853842a82e3dd85e5973fc4ab615ae23dba316
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-23 22:19:31 +0300

    Added the upload media functionality

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-
+    thumb = models.ImageField(default='default.png', blank=True)
 
     def __str__(self):
         return self.title
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'articles/static'),
 ]
 
+# managing file uploads and serve media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
