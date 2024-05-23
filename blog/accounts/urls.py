89a9d1da57835a4da156e490c187f1747c90461a
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-23 23:39:54 +0300

    Added the signup template, url pattern and view

---
+++ blog/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('signup/', views.signup_view, name="signup"),
+]
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
