efc94976cdd4bd684368807a35a0dd6ad4ffdb02
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-24 10:24:10 +0300

    embedded the csrf token in my sign up form

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+             form.save()
+             #  log the user in
+             return redirect('articles:article_list')
+    else:
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
