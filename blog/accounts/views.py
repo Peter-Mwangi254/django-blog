7d69924611f30fac1bc2aa17e1254bd0fc708766
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-24 11:20:30 +0300

    created the login form, and the url to render the login page

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def signup_view(request):
     if request.method == 'POST':
@@ -11,3 +11,14 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log user
+            return redirect('articles:article_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
