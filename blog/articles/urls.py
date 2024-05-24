06b8b42790a31ba7d09df8f8b8e7898a3af08e68
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-24 23:06:46 +0300

    added the login required decorator for only login users to access the create article  page

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -6,5 +6,6 @@ app_name = 'articles'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('articles/', views.article_list, name='article_list'),
+    path('articles/create/', views.article_create, name='article_create'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
+from django.contrib.auth.decorators import login_required
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -8,3 +9,7 @@ def article_list(request):
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
