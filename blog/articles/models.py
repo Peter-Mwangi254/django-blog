1afa7e85b9008148225acf0a85bbf46593c17260
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-28 00:32:06 +0300

    added a model form

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -6,6 +7,12 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from .import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -12,4 +13,11 @@ def article_detail(request, slug):
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            # save article to db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', { 'form': form })
