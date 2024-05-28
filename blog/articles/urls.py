1ad8a304c5e9f8cc68b26aca7ad551440fda0108
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-29 00:15:55 +0300

    added a redirection to the homepage

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path('', views.article_list, name='home'),
     path('articles/', views.article_list, name='article_list'),
     path('articles/create/', views.article_create, name='article_create'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('', include('django.contrib.auth.urls')),
 ]
